4048e4970752447d543e30c814ee0c5fc478e3c7
esakaty <esakat.ekusuy@gmail.com>
2023-01-21 01:13:37 +0900

    Wordフッター書き換え処理実装

--- Consistent_Header_Footer/MainWindow.xaml.cs
+++ Consistent_Header_Footer/MainWindow.xaml.cs
@@ -13,16 +13,74 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace Consistent_Header_Fotter
+using Microsoft.Office.Interop.Word;
+using static System.Collections.Specialized.BitVector32;
+
+namespace Consistent_Header_Footer
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : System.Windows.Window
     {
+        enum e_OperateResult{
+            Error,
+            Complete,
+            Continue
+        };
+
         public MainWindow()
         {
             InitializeComponent();
+            this.txtPathFolder.Text = System.IO.Path.GetDirectoryName(
+                System.Reflection.Assembly.GetExecutingAssembly().Location) + "\\TargetFolder";
+        }
+
+        private void btnStart_Click(object sender, RoutedEventArgs e)
+        {
+            e_OperateResult result = e_OperateResult.Continue;
+            result = UpdateFooter_Word(this.txtPathFolder.Text + "\\TargetFile1.docx",2,10);
+        }
+
+        private e_OperateResult UpdateFooter_Word(string strFilePath,int startPageNum,int totalPageNum)
+        {
+            e_OperateResult result = e_OperateResult.Error;
+            if (System.IO.File.Exists(this.txtPathFolder.Text + "\\TargetFile1.docx")) {
+                // Wordアプリケーションを起動
+                var wordApp = new Microsoft.Office.Interop.Word.Application();
+
+                // 新しい文書を開く
+                var doc = wordApp.Documents.Open(strFilePath);
+
+                // 全セクションに対して
+                foreach (Microsoft.Office.Interop.Word.Section section in doc.Sections)
+                {   
+                    if (section.Index == 0)
+                    {
+                        //開始番号を変更
+                        section.Headers[WdHeaderFooterIndex.wdHeaderFooterFirstPage].PageNumbers.RestartNumberingAtSection = true;
+                        section.Headers[WdHeaderFooterIndex.wdHeaderFooterFirstPage].PageNumbers.StartingNumber = 2;
+                    }
+
+                    // フッターを取得
+                    var primaryFooter = section.Footers[WdHeaderFooterIndex.wdHeaderFooterPrimary];
+
+                    // フッターにページ番号フィールドを埋め込む
+                    primaryFooter.Range.Fields.Add(primaryFooter.Range, WdFieldType.wdFieldPage);
+                    primaryFooter.Range.InsertBefore("( ");
+                    primaryFooter.Range.InsertAfter(" / " + totalPageNum + " )");
+
+                }
+
+                // 更新した文書を保存
+                doc.Save();
+
+                // Wordを終了
+                wordApp.Quit();
+            }
+
+            return result;
         }
     }
+
 }
