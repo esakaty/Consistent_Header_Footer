d85d96ae75f6fb0ac2a20a15e732c33581afe212
esakaty <esakat.ekusuy@gmail.com>
2023-01-26 23:28:17 +0900

    MVVMに変更。 スレッド化未

--- Consistent_Header_Footer/MainWindow.xaml.cs
+++ Consistent_Header_Footer/MainWindow.xaml.cs
@@ -1,12 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Configuration;
 using System.IO;
 using System.Linq;
 using System.Printing;
+using System.Reflection;
 using System.Text;
 using System.Text.RegularExpressions;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -17,8 +20,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-
-
+using System.Windows.Threading;
 using Microsoft.Office.Interop.Word;
 using static System.Collections.Specialized.BitVector32;
 
@@ -29,62 +31,211 @@ namespace Consistent_Header_Footer
     /// </summary>
     public partial class MainWindow : System.Windows.Window
     {
-        enum e_OperateResult{
-            Error,
-            Complete,
-            Continue
-        };
-
-        public class FileData
-        {
-            public enum eState
-            {
-                Wait,
-                Operate,
-                Stop,
-                Complete,
-                Skip,
-                Error
-            }
-
-            public String Path ="";
-            public int Group = 0;
-            public int No = 0;
-            public int StartPage = 0;
-            public int TotalPage = 0;
-            public int GroupTotalPage = 0;
-            public eState State = eState.Skip;
-            public bool Selected = false;
-        }
-
-        public class GroupData
-        {
-            public string Name = "";
-            public String Key = "";
-            public int Count = 0;
-        }
-
-        public List<FileData> fileDatas { get; set; } = new List<FileData>();
-        public List<GroupData> groupList { get; set; } = new List<GroupData>();
-
         public MainWindow()
         {
             InitializeComponent();
-            this.txtPathFolder.Text = System.IO.Path.GetDirectoryName(
-                System.Reflection.Assembly.GetExecutingAssembly().Location) + "\\TargetFolder";
+        }
+    }
+    public enum eState
+    {
+        Wait,
+        Operate,
+        Stop,
+        Complete,
+        Skip,
+        Error
+    }
+    public class FileData
+    {
+        public String Path { get; set; } = "";
+        public int Group { get; set; }
+        public int No { get; set; }
+        public int StartPage { get; set; }
+        public int TotalPage { get; set; }
+        public int GroupTotalPage { get; set; }
+        public eState State { get; set; }
+        public bool Selected { get; set; }
+    }
+    public class GroupData
+    {
+        public string Name = "";
+        public String Key = "";
+        public int Count = 0;
+        public int GroupTotalPage = 0;
+    }
+
+    ///============================================================
+    /// ViewModel
+    ///============================================================
+    public class Main_ViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+        public ObservableCollection<FileData> FileDatas { get; set; }
+        public List<GroupData> groupList { get; set; } = new List<GroupData>();
+        public String PathFolder { get; set; }
+
+        public String ValuePathFolder
+        {
+            get
+            {
+                return PathFolder;
+            }
+            set
+            {
+                PathFolder = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(ValuePathFolder)));
+            }
+        }
+        public ObservableCollection<FileData> ValueFileDataCollection
+        {
+            get
+            {
+                return FileDatas;
+            }
+            set
+            {
+                FileDatas = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(ValueFileDataCollection)));
+            }
         }
 
-        private void btnStart_Click(object sender, RoutedEventArgs e)
+        public Command_Buttons Command_Buttons { get;  set; }
+
+        public Main_ViewModel()
         {
-            Update_All_Files();
+            Command_Buttons = new Command_Buttons(this);
+            FileDatas = new ObservableCollection<FileData>();
+            PathFolder = System.IO.Path.GetDirectoryName(                System.Reflection.Assembly.GetExecutingAssembly().Location) + "\\TargetFolder";
+        }
+    }
+
+    public class Command_Buttons : ICommand
+    {
+        private Main_ViewModel _view { get; set; }
+        public Command_Buttons(Main_ViewModel view) { _view = view; }
+
+        public event EventHandler? CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
         }
 
-        private void Update_All_Files()
+        public bool CanExecute(object? parameter) { return true; }
+
+        public void Execute(object? parameter)
         {
-            foreach (FileData file in fileDatas)
+            switch (parameter)
+            {
+                case "btnOpen":
+                    OpenFolder();
+                    break;
+
+                case "btnChek":
+                    CheckFolder();
+                    break;
+
+                case "btnStart":
+                    StartFolder();
+                    break;
+
+                default:
+                    break;
+            }
+        }
+
+
+        private void OpenFolder()
+        {
+            try
+            {
+                ObservableCollection<FileData> tmp_fileDatas = new();
+
+                var files = Directory.GetFiles(_view.PathFolder);
+                var fileNames = files.Select(file => System.IO.Path.GetFileName(file));
+                foreach (var (fileName, index) in fileNames.Select((filename, index) => (filename, index)))
+                {
+                    tmp_fileDatas.Add(new FileData()
+                    {
+                        Path = fileName,
+                    });
+                }
+                _view.ValueFileDataCollection = tmp_fileDatas;
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.WriteLine(ex);
+            }
+        }
+
+        private void CheckFolder()
+        {
+            foreach (var fileData in _view.ValueFileDataCollection)
+            {
+                GetPageNum(fileData);
+            }
+            foreach (var fileData in _view.ValueFileDataCollection)
+            {
+                GetGroup(fileData);
+            }
+            foreach (var data in _view.ValueFileDataCollection)
+            {
+                data.GroupTotalPage = _view.groupList[data.Group].GroupTotalPage;
+            }
+        }
+
+        private void StartFolder()
+        {
+            foreach (FileData file in _view.ValueFileDataCollection)
             {
                 UpdateFooter_Word(file);
             }
+        }
+
+        private void GetPageNum(FileData file)
+        {
+            if (!string.IsNullOrEmpty(file.Path))
+            {
+                var wordApp = new Microsoft.Office.Interop.Word.Application();
+                var doc = wordApp.Documents.Open(_view.PathFolder + "\\" + file.Path);
+
+                file.TotalPage = doc.ComputeStatistics(WdStatistic.wdStatisticPages);
+                doc.Close();
+                // todo 何かを待たないと最後のファイルがエラーになる。
+                System.Threading.Thread.Sleep(500);
+                wordApp.Quit();
+            }
+        }
+
+        private void GetGroup(FileData file)
+        {
+            bool flg = false;
+            int tmpPageNum = 1;
+            string Group_Name = "";
+
+            foreach (var (Group, index) in _view.groupList.Select((GroupData, Index) => (GroupData, Index)))
+            {
+                if (Group_Name == Group.Name)
+                {
+                    flg = true;
+                    tmpPageNum = Group.GroupTotalPage + 1;
+
+                    file.Group = index;
+                    file.No = Group.Count + 1;
+                    Group.Count++;
+                    Group.GroupTotalPage += file.TotalPage;
+                    break;
+                }
+            }
+            if (flg != true)
+            {
+                _view.groupList.Add(new GroupData()
+                {
+                    Name = Group_Name,
+                    GroupTotalPage = file.TotalPage,
+                    Key = ""
+                });
+            }
+            file.StartPage = tmpPageNum;
         }
 
         private void UpdateFooter_Word(FileData file)
@@ -92,10 +243,10 @@ namespace Consistent_Header_Footer
             Microsoft.Office.Interop.Word.Application? wordApp = null;
             Microsoft.Office.Interop.Word.Document? doc = null;
 
-            file.State = FileData.eState.Operate;
+            file.State = eState.Operate;
             try
             {
-                System.Diagnostics.Debug.WriteLine("Start\n Path="+file.Path);
+                System.Diagnostics.Debug.WriteLine("Start\n Path=" + file.Path);
 
                 // Wordアプリケーションを起動
                 wordApp = new Microsoft.Office.Interop.Word.Application();
@@ -129,7 +280,7 @@ namespace Consistent_Header_Footer
                 // 更新した文書を保存
                 doc.Save();
                 System.Diagnostics.Debug.WriteLine(" Save File");
-                file.State = FileData.eState.Complete;
+                file.State = eState.Complete;
 
                 // todo 何かを待たないと最後のファイルがエラーになる。
                 System.Threading.Thread.Sleep(300);
@@ -137,7 +288,7 @@ namespace Consistent_Header_Footer
             catch (Exception ex)
             {
                 System.Diagnostics.Debug.WriteLine(ex);
-                file.State = FileData.eState.Error;
+                file.State = eState.Error;
             }
             finally
             {
@@ -153,110 +304,6 @@ namespace Consistent_Header_Footer
                     // Wordを終了
                     wordApp.Quit();
                 }
-            }
-        }
-
-        private void btnOpen_Click(object sender, RoutedEventArgs e)
-        {
-            try
-            {
-                List<FileData> tmp_fileDatas = new List<FileData>();
-
-                var files = Directory.GetFiles(this.txtPathFolder.Text);
-                var fileNames = files.Select(file => System.IO.Path.GetFullPath(file));
-                foreach (var (fileName, index) in fileNames.Select((filename, index) => (filename, index)))
-                {
-                    tmp_fileDatas.Add(new FileData()
-                    {
-                        Path = fileName,
-                        No = index,
-                        State = FileData.eState.Wait
-                    });
-                    UpdateFileData(tmp_fileDatas[index]);
-                }
-                fileDatas = tmp_fileDatas;
-
-                foreach(var data in fileDatas)
-                {
-                    bool flg = false;
-                    int tmpPageNum = 1;
-                    string Group_Name = "";
-
-                    foreach (var (Group,index) in groupList.Select((GroupData,Index) => (GroupData,Index)))
-                    {
-                        if(Group_Name == Group.Name)
-                        {
-                            flg = true;
-                            tmpPageNum = Group.Count + 1;
-
-                            data.Group = index;
-                            Group.Count+=data.TotalPage;
-                            break;
-                        }
-                    }
-                    if(flg != true)
-                    {
-                        groupList.Add(new GroupData()
-                        {
-                            Name = Group_Name,
-                            Count = data.TotalPage,
-                            Key = ""
-                        });
-                    }
-                    data.StartPage = tmpPageNum;
-                }
-
-                foreach (var data in fileDatas)
-                {
-                    data.GroupTotalPage = groupList[data.Group].Count;
-                }
-
-                System.Data.DataTable dt = new System.Data.DataTable();
-                dt = new System.Data.DataTable();
-                dt.Columns.Add("FileName");
-                dt.Columns.Add("Group");
-                dt.Columns.Add("No");
-                dt.Columns.Add("StartPage");
-                dt.Columns.Add("TotalPage");
-                dt.Columns.Add("GroupTotalPage");
-                dt.Columns.Add("State");
-                dt.Columns.Add("Select");
-
-                for (int i = 0; i < fileDatas.Count; i++)
-                {
-                    System.Data.DataRow row = dt.NewRow();
-                    row[0] = System.IO.Path.GetFileName(fileDatas[i].Path);
-                    row[1] = fileDatas[i].Group;
-                    row[2] = fileDatas[i].No;
-                    row[3] = fileDatas[i].StartPage;
-                    row[4] = fileDatas[i].TotalPage;
-                    row[5] = groupList[fileDatas[i].Group].Count;
-                    row[6] = fileDatas[i].State;
-                    row[7] = fileDatas[i].Selected;
-                    dt.Rows.Add(row);
-                }
-
-                dataGrid_FileInfo.DataContext = dt;
-            }
-            catch(Exception ex)
-            {
-                System.Diagnostics.Debug.WriteLine(ex);
-            }
-        }
-
-        private void UpdateFileData(FileData file)
-        {
-            if (!string.IsNullOrEmpty(file.Path))
-            {
-                var folderPath = file.Path;
-                var wordApp = new Microsoft.Office.Interop.Word.Application();
-                var pageCounts = new Dictionary<string, int>();
-                var doc = wordApp.Documents.Open(file.Path);
-                file.TotalPage = doc.ComputeStatistics(WdStatistic.wdStatisticPages);
-                doc.Close();
-                // todo 何かを待たないと最後のファイルがエラーになる。
-                System.Threading.Thread.Sleep(500);
-                wordApp.Quit();
             }
         }
     }
